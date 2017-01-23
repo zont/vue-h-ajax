d5ed47c5bb67cc17d1fb9e4eced4898b225a7ec2
Alexander Zonov <alexander.zonov@developonbox.ru>
2017-01-23 23:15:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# vue-h-ajax
+[![npm version](https://badge.fury.io/js/vue-h-ajax.svg)](https://badge.fury.io/js/vue-h-ajax)
+[![npm](https://img.shields.io/npm/l/express.svg)](http://opensource.org/licenses/MIT)
+[![Build Status](https://travis-ci.org/zont/vue-h-ajax.svg?branch=master)](https://travis-ci.org/zont/vue-h-ajax)
+
+> Edge 12+, FF 36+, Chrome 49+, or use translation from ES2105 to ES5
+
+## Introduction
+
+`vue-h-ajax` is the small ajax module for [Vue.js](http://vuejs.org). It deeply integrates with Vue.js core to make building Single Page Applications with Vue.js a breeze. Features include:
+
+- Lightweight: ???kb of sources
+- No dependencies
+- try to parse data as JSON by default
+- withCredentials by default
+- DELETE request with body
+
+## Setup
+```bash
+npm install vue-h-ajax
+```
+
+## Example
+```javascript
+import Vue from 'vue';
+import ajax from 'vue-h-ajax';
+
+
+Vue.use(ajax);
+
+
+Vue.http.get('/backend/users')
+  .then(users => {
+    console.log(users);
+  })
+  .catch(e => console.error(e));
+
+Vue.http.stop('/backend/users');
+
+
+const app = new Vue({
+  template: `
+    <div class="main">
+      <component :is="$route.component"/>
+    </div>
+  `,
+  el: '#app',
+  created() {
+    this.$http.post('/backend/user', {name: 'Bill'}, {'Content-Type': 'application/json'})
+      .then(addedUser => {
+        console.log(addedUser.id);
+      })
+      .catch(e => console.error(e));
+  }
+});
+```
+
+## API
+
+#### withCredentials
+  - type: `Boolean`. Default `true`
+```javascript
+import Vue from 'vue';
+import ajax from 'vue-h-ajax';
+
+
+Vue.use(ajax);
+
+Vue.http.withCredentials = false;
+```
+
+#### headers
+  - type: `Object`
+
+Global headers for all requests
+```javascript
+import Vue from 'vue';
+import ajax from 'vue-h-ajax';
+
+
+Vue.use(ajax);
+
+Vue.http.headers = {
+  'Content-Type': 'application/json'
+};
+```
+
+#### get(url, data, headers), post(...), put(...), delete(...)
+  - returns `Promise`
+
+#### stop(url)
+Abort all current requests matched **url**
+```javascript
+import Vue from 'vue';
+import ajax from 'vue-h-ajax';
+
+
+Vue.use(ajax);
+
+
+Vue.http.get('/backend/users');
+Vue.http.get('/backend/users');
+Vue.http.get('/backend/users');
+
+Vue.http.stop('/backend/users');
+```
---
+++ index.js
@@ -0,0 +1,152 @@
+const CODE_SUCCESS = 200;
+const CODE_FAIL = 300;
+const requests = {};
+
+
+function instanceOf(src, name) {
+  try {
+    return src instanceof window[name];
+  } catch (e) {
+    return false;
+  }
+}
+
+function parseRequestData(params) {
+  if (
+    // ArrayBufferView
+  instanceOf(params, 'Int8Array') ||
+  instanceOf(params, 'Uint8Array') ||
+  instanceOf(params, 'Uint8ClampedArray') ||
+  instanceOf(params, 'Int16Array') ||
+  instanceOf(params, 'Uint16Array') ||
+  instanceOf(params, 'Int32Array') ||
+  instanceOf(params, 'Uint32Array') ||
+  instanceOf(params, 'Float32Array') ||
+  instanceOf(params, 'Float64Array') ||
+  instanceOf(params, 'DataView') ||
+  // Blob
+  instanceOf(params, 'Blob') ||
+  // Document
+  instanceOf(params, 'HTMLDocument') ||
+  instanceOf(params, 'XMLDocument') ||
+  // DOMString
+  instanceOf(params, 'String') ||
+  typeof params === 'string' ||
+  // FormData
+  instanceOf(params, 'FormData')
+  ) {
+    return params;
+  }
+
+  return JSON.stringify(params);
+}
+
+function parseResponseData(data, defaults) {
+  try {
+    return JSON.parse(data);
+  } catch (e) {
+    return defaults;
+  }
+}
+
+function makeGetUrl(url, data) {
+  return url + (url.indexOf('?') !== -1 ? '&' : '?') + Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+}
+
+function request(method, url, data = null, headers = {}, withCredentials) {
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = withCredentials;
+
+  const promise = new Promise((resolve, reject) => {
+    xhr.open(method, url, true);
+
+    Object.keys(headers).forEach(name => xhr.setRequestHeader(name, headers[name]));
+
+    xhr.addEventListener('timeout', reject);
+    xhr.addEventListener('abort', reject);
+    xhr.addEventListener('error', reject);
+    xhr.addEventListener('load', () => {
+      requests[url].splice(requests[url].indexOf(promise), 1);
+
+      if (CODE_SUCCESS <= xhr.status && xhr.status < CODE_FAIL) {
+        resolve(parseResponseData(xhr.response, xhr.response));
+      } else {
+        reject(parseResponseData(xhr.response, xhr));
+      }
+    });
+  });
+
+  promise.abort = xhr.abort.bind(xhr);
+
+  xhr.send(data);
+
+  return promise;
+}
+
+function add(url, request) {
+  requests[url] = requests[url] || [];
+  requests[url].push(request);
+  return request;
+}
+
+
+const ajax = {
+  headers: {},
+
+  withCredentials: true,
+
+  get(url, data, headers) {
+    return add(url, request(
+      'GET',
+      data ? makeGetUrl(url, data) : url,
+      {},
+      Object.assign({}, this.headers, headers),
+      this.withCredentials
+    ));
+  },
+
+  post(url, data, headers) {
+    return add(url, request(
+      'POST',
+      url,
+      parseRequestData(data),
+      Object.assign({}, this.headers, headers),
+      this.withCredentials
+    ));
+  },
+
+  put(url, data, headers) {
+    return add(url, request(
+      'PUT',
+      url,
+      parseRequestData(data),
+      Object.assign({}, this.headers, headers),
+      this.withCredentials
+    ));
+  },
+
+  delete(url, data, headers) {
+    return add(url, request(
+      'DELETE',
+      url,
+      parseRequestData(data),
+      Object.assign({}, this.headers, headers),
+      this.withCredentials
+    ));
+  },
+
+  stop(url) {
+    requests[url].forEach(request => request.abort());
+    requests[url] = [];
+  }
+};
+
+
+module.exports = {
+  install(Vue) {
+    Vue.http = ajax;
+    Object.defineProperty(Vue.prototype, '$http', {get: () => ajax});
+  }
+};
